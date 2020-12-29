315a314ed1ca4a3fecf56ca9e6b495a7cf5766e4
Guillaume Chau <guillaume.b.chau@gmail.com>
2020-12-29 20:26:23 +0100

    chore(demo): fix eslint config

--- packages/demo/.eslintrc.js
+++ packages/demo/.eslintrc.js
@@ -1,7 +1,18 @@
 module.exports = {
+  root: true,
+
+  env: {
+    node: true,
+  },
+
   extends: [
+    'plugin:vue/essential',
     '@vue/standard',
   ],
+
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
 
   rules: {
     'comma-dangle': ['error', 'always-multiline'],
